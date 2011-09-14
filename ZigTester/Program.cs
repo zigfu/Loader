8a314503468013b98b6e0d84696869bc86750e72
Roee Shenberg <roee@zigfu.com>
2011-09-13 18:20:13 -0700

    tester for ZigLib

---
+++ ZigTester/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZigLib;
+namespace ZigTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Installed Zigs:");
+            InstalledZig last = null;
+            foreach (InstalledZig zig in ZigLib.ZigLib.EnumerateInstalledZigs()) {
+                Console.WriteLine(zig);
+                last = zig;
+            }
+            Console.WriteLine("Network Zigs:");
+            foreach (RemoteZig zig in ZigLib.ZigLib.EnumerateRemoteZigs()) {
+                Console.WriteLine(zig);
+            }
+            if (!((args.Length > 0) && (args[0] == "shit"))) {
+                if (null != last) {
+                    Console.WriteLine("Doing shit (Launching last enumerated zig)");
+                    last.Launch();
+                }
+                else {
+                    Console.WriteLine("Sorry, but the princess is in another castle (there are no installed zigs)");
+                }
+            }
+        }
+    }
+}
