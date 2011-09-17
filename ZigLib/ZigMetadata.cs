c274fecb5711e4f5d9761ce1083a7e2b9c61f5da
Roee Shenberg <roee@zigfu.com>
2011-09-16 20:52:47 -0700

    Added reading ZigID

--- ZigLib/ZigMetadata.cs
+++ ZigLib/ZigMetadata.cs
@@ -9,6 +9,7 @@ namespace ZigLib
         public string Developer { get; private set; }
         public string Description { get; private set; }
         public string ThumbnailURI { get; private set; }
+        public string ID { get; private set; }
 
         public static ZigMetadata FromFile(string path)
         {
@@ -18,12 +19,14 @@ namespace ZigLib
         const string NAME = "name";
         const string THUMBNAIL = "thumbnail_url";
         const string DESCRIPTION = "description";
+        const string ZIG_ID = "zigid";
 
         public ZigMetadata(Hashtable properties)
         {
             Name = (string)properties[NAME];
             Description = (string)properties[DESCRIPTION];
             ThumbnailURI = (string)properties[THUMBNAIL];
+            ID = (string)properties[ZIG_ID];
 
             Developer = "DevCo"; //TODO: real data
         }
