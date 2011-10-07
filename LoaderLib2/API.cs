22439a55ca0096e6cc7894d5b51f817759ee9e8c
Roee Shenberg <roee@zigfu.com>
2011-10-06 17:23:19 -0700

    forgot to check in, d'oh

---
+++ LoaderLib2/API.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LoaderLib2
+{
+    public class API
+    {
+        internal static UberLib _Uber;
+        internal static UberLib Uber
+        {
+            get
+            {
+                if (null == _Uber) {
+                    _Uber = new UberLib();
+                    _Uber.WaitTillInit();
+                }
+                return _Uber;
+            }
+        }
+
+
+        static public void LaunchProcess(string Command, string WorkingDirectory, OpenNI.Context Context)
+        {
+            if (Uber != null) {
+                Console.WriteLine(":)");
+                //throw new Exception("hurrah");
+                Uber.LaunchProcess(Command, WorkingDirectory, Context);
+            }
+        }
+    }
+}
